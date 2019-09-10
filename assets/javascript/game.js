f8243eb096a106e3ab1af9abb0e1ba4131108449
Angelica Zamer <angelica.zamer@gmail.com>
2019-09-10 15:52:16 -0500

    vars setup

---
+++ assets/javascript/game.js
@@ -0,0 +1,24 @@
+var crystal = {
+    blueCrystal: {
+        name: "blue",
+        value: 0
+    },
+    redCrystal: {
+        name: "red",
+        value: 0
+    },
+    yellowCrystal: {
+        name: "yellow",
+        value: 0
+    },
+    greenCrystal: {
+        name: "green",
+        value: 0
+    }
+};
+
+var wins = 0;
+var losses = 0;
+var currentScore=0;
+var goal=0;
+

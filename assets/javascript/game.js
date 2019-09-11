573593ff7ad38d917b5e8ffe86fb6f3b1cae991a
Angelica Zamer <angelica.zamer@gmail.com>
2019-09-11 10:03:39 -0500

    function set for target score & crystal values

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,24 +1,44 @@
-var crystal = {
-    blueCrystal: {
+var crystal = [
+    {Blue: {
         name: "blue",
         value: 0
-    },
-    redCrystal: {
+    }},
+    {Red: {
         name: "red",
         value: 0
-    },
-    yellowCrystal: {
+    }},
+    {Green: {
         name: "yellow",
         value: 0
-    },
-    greenCrystal: {
+    }},
+    {Yellow: {
         name: "green",
         value: 0
-    }
-};
+    }}
+];
 
 var wins = 0;
 var losses = 0;
 var currentScore=0;
 var goal=0;
 
+function startUp() {
+    //chooose random score num
+    goal = Math.floor(Math.random()*120)+19;
+    console.log(goal);
+    //random crystal val
+
+    //set vars to 0 
+}
+
+function crystalValue (crystal) {
+    crystal[0].Blue.value = Math.floor(Math.random()*12)+1;
+    crystal[1].Red.value = Math.floor(Math.random()*12)+1;
+    crystal[2].Green.value = Math.floor(Math.random()*12)+1;
+    crystal[3].Yellow.value = Math.floor(Math.random()*12)+1;
+    console.log(crystal[0], crystal[1],crystal[2],crystal[3],);
+}
+
+
+crystalValue(crystal);
+// startUp();
